6b7ce4dec4550c4ef282f4b9fee7d1931b9641c9
Matt Thomas <mthomas8@gmail.com>
2024-12-05 09:29:24 -0500

    working 4b

--- 4b.py
+++ 4b.py
@@ -10,11 +10,19 @@ with open("4.txt", "r") as file:
 for vertical_index in range(len(all_lines)):
   line = all_lines[vertical_index]
 
-  for index, char in enumerate(line):
-    # Only check starting on line 1 and ending on the penultimate line.
-    if 1 <= vertical_index < len(all_lines) and char == 'A':
-       # TODO Check above for "M S" and "S M"
-       # TODO Check below for "M S" and "S M"
+  # Only check starting on line 1 and ending on the penultimate line.
+  if 1 <= vertical_index < (len(all_lines) - 1):
+    for index, char in enumerate(line):
+      if 1 <= index < len(line) - 1 and char == 'A':
+        # print(f"Checking line {vertical_index}, char {index}")
+
+        # Extract above and below snippets
+        diag1 = all_lines[vertical_index - 1][index - 1] + char + all_lines[vertical_index + 1][index + 1]
+        diag2 = all_lines[vertical_index + 1][index - 1] + char + all_lines[vertical_index - 1][index + 1]
+
+        possible_values = ["MAS", "SAM"]
+        if diag1 in possible_values and diag2 in possible_values:
+          xmas_appearances += 1
 
 print(xmas_appearances)
 

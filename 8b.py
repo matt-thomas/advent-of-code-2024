eab5fdbe209697c9f6a1f2b8a9dcc5d3e05ad03e
Matt Thomas <mthomas8@gmail.com>
2024-12-09 11:36:41 -0500

    working 8b

--- 8b.py
+++ 8b.py
@@ -1,57 +1,77 @@
 import itertools
+from math import gcd
 
 map_lines = []
 antinode_positions = []
 
-with open("8-test.txt", "r") as file:
+with open("8.txt", "r") as file:
     for line in file:
-      map_lines.append(line.strip())
+        map_lines.append(line.strip())
 
 # Assemble an index of grid coordinates by symbol.
 symbols_index = {}
 map_width = len(map_lines[0])
+print(map_width)
 map_height = len(map_lines)
+print(map_height)
 
 for y, row in enumerate(map_lines):
-  for x, symbol in enumerate(row):
-    if symbol != ".":
-      try:
-        symbols_index[symbol].append((x, y))
-      except KeyError:
-        symbols_index[symbol] = [(x, y)]
+    for x, symbol in enumerate(row):
+        if symbol != "." and symbol != "#":
+            try:
+                symbols_index[symbol].append((x, y))
+            except KeyError:
+                symbols_index[symbol] = [(x, y)]
 
-# The signal only applies its nefarious effect at specific antinodes based on the resonant frequencies of the antennas. In particular, an antinode occurs at any point that is perfectly in line with two antennas of the same frequency - but only when one of the antennas is twice as far away as the other. This means that for any pair of antennas with the same frequency, there are two antinodes, one on either side of them.
 for symbol, positions in symbols_index.items():
-  for (x1, y1), (x2, y2) in itertools.combinations(positions, 2):
-      print(f"First item {x1}, {y1}")
-      print(f"Second item {x2}, {y2}")
+    for (x1, y1), (x2, y2) in itertools.combinations(positions, 2):
+        print(f"First item {x1}, {y1}")
+        print(f"Second item {x2}, {y2}")
 
-      diff1 = (x1 - x2, y1 - y2)
-      diff2 = (x2 - x1, y2 - y1)
+        dx = x2 - x1
+        dy = y2 - y1
+        g = gcd(dx, dy)
+        dx_step = dx // g
+        dy_step = dy // g
 
-      pos = (x1, y1)
-      while True:
-          # Move in the direction of diff1
-          pos = tuple(x + y for x, y in zip(pos, diff1))
+        # Use dx_step, dy_step in both directions starting from each antenna
 
-          # Check if within the map boundaries
-          if 0 < (pos[0] + 1) <= map_width and 0 < (pos[1] + 1) <= map_height:
-              antinode_positions.append(pos)
-          else:
-              # As soon as we exceed the boundary, stop
-              break
+        # From antenna (x1, y1) forward
+        pos = (x1, y1)
+        while True:
+            pos = (pos[0] + dx_step, pos[1] + dy_step)
+            if 0 <= pos[0] < map_width and 0 <= pos[1] < map_height:
+                antinode_positions.append(pos)
+            else:
+                break
 
-      pos = (x2, y2)
-      while True:
-          # Move in the direction of diff2
-          pos = tuple(x + y for x, y in zip(pos, diff2))
+        # From antenna (x1, y1) backward
+        pos = (x1, y1)
+        while True:
+            pos = (pos[0] - dx_step, pos[1] - dy_step)
+            if 0 <= pos[0] < map_width and 0 <= pos[1] < map_height:
+                antinode_positions.append(pos)
+            else:
+                break
 
-          # Check if within the map boundaries
-          if 0 < (pos[0] + 1) <= map_width and 0 < (pos[1] + 1) <= map_height:
-              antinode_positions.append(pos)
-          else:
-              # As soon as we exceed the boundary, stop
-              break
+        # From antenna (x2, y2) forward
+        pos = (x2, y2)
+        while True:
+            pos = (pos[0] + dx_step, pos[1] + dy_step)
+            if 0 <= pos[0] < map_width and 0 <= pos[1] < map_height:
+                antinode_positions.append(pos)
+            else:
+                break
 
+        # From antenna (x2, y2) backward
+        pos = (x2, y2)
+        while True:
+            pos = (pos[0] - dx_step, pos[1] - dy_step)
+            if 0 <= pos[0] < map_width and 0 <= pos[1] < map_height:
+                antinode_positions.append(pos)
+            else:
+                break
+
+# Convert to a set for uniqueness
 antinode_positions = set(antinode_positions)
 print(len(antinode_positions))
